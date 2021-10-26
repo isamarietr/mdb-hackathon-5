a72241552a18c5512be0a7e86d76738ea75aceea
ctselebis007 <81821852+ctselebis007@users.noreply.github.com>
2021-10-26 16:02:17 -0400

    Add files via upload

---
+++ scripts/ReadTempTS1.py
@@ -0,0 +1,25 @@
+from settings import COLLECTION_TS_NAME, DB_NAME, CLUSTER_URL
+from faker import Faker
+import pymongo
+import random
+import datetime
+import time
+import requests
+import sys
+
+
+fake = Faker()
+
+connection = pymongo.MongoClient(CLUSTER_URL)
+db = connection[DB_NAME]
+
+while True:
+    DeviceValue = random.randrange(50, 150)
+
+    sensorDate = datetime.datetime.utcnow()
+    
+    document = { 'DeviceDate': sensorDate, 'meta' : { 'DeviceId': 'RoomTemp1234', 'DeviceType': 'RoomTemparature Monitor', 'UserId': '61780e2968f275f84ec66f2c'}, 'value': DeviceValue }
+    
+    db.IoT_TS_Data.insert_one(document)
+    
+    time.sleep(10); 
