e46451d2fbf2e7e0096d557193435d90f375dfc3
ctselebis007 <81821852+ctselebis007@users.noreply.github.com>
2021-10-26 13:35:30 -0400

    Add files via upload

---
+++ scripts/_settings.py
@@ -0,0 +1,6 @@
+# Database name to use
+DB_NAME = 'Your_DB_Name'
+COLLECTION_NAME = 'You_Collection_Name'
+COLLECTION_TS_NAME = 'You_Collection_TS_Name'
+# cluster connection string
+CLUSTER_URL = 'YOUR_CLUSTER_URI'
